971911afb714ad3a19c18e51ce1efa9c658353b4
terrytyk77 <terrytyk77@gmail.com>
2022-09-25 20:16:36 +0100

    Bug Fix: Smoke not shown when no hit

--- Source/Shooter/Item.cpp
+++ Source/Shooter/Item.cpp
@@ -9,6 +9,7 @@
 
 // Sets default values
 AItem::AItem()
+	: ItemName(FString("Default Item"))
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
