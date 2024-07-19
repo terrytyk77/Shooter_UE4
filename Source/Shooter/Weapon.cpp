92666e7ce15b5a2bd02ee23533af9b64a7ae25f0
Terry Keyrouz <terrytyk77@gmail.com>
2024-07-19 12:55:42 +0100

    Added muzzle flash, auto fire rate and fire sound per weapon in the data table

--- Source/Shooter/Weapon.cpp
+++ Source/Shooter/Weapon.cpp
@@ -115,6 +115,9 @@ void AWeapon::OnConstruction(const FTransform& Transform)
 			CrosshairRight = WeaponDataRow->CrosshairRight;
 			CrosshairBottom = WeaponDataRow->CrosshairBottom;
 			CrosshairTop = WeaponDataRow->CrosshairTop;
+			AutoFireRate = WeaponDataRow->AutoFireRate;
+			MuzzleFlash = WeaponDataRow->MuzzleFlash;
+			FireSound = WeaponDataRow->FireSound;
 
 			if (GetMaterialInstance())
 			{
